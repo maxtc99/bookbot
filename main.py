494aae0bb21ed0fe58c57d4abe89b51c699e4b1c
maxtc99 <max.tc99@gmail.com>
2025-10-22 17:47:59 +0100

    Added char count to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 def get_book_text(filepath):
     with open(filepath) as f:
         text = f.read()
@@ -10,4 +10,5 @@ def main():
     
     words = get_num_words(get_book_text("books/frankenstein.txt"))
     print(f"Found {words} total words")
+    print(get_num_char(get_book_text("books/frankenstein.txt")))
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,20 @@ def get_num_words(text):
         counter += 1 
     print(f"Found {counter} total words") '''
     return len(words)
+
+def get_num_char(text):
+    words = text.split()
+    dict = {}
+    counter = 1
+    for word in words:
+        lword = word.lower()
+        for char in lword:                       
+            if char in dict:
+                dict[char] += 1
+            else:
+                dict.update({char:counter})  
+    return dict
+            
+            # if char exists in dictionary then dict value += 1
+            # else 
+            # dict add  key / value
